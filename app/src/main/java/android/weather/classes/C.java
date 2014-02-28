3f9e9f000532f683b04b2fbad85affd50d81e673
sergius-m <p37sergius@gmail.com>
2014-02-28 17:47:54 +0300

    Constants

---
+++ app/src/main/java/android/weather/classes/C.java
@@ -0,0 +1,8 @@
+package android.weather.classes;
+
+
+public class C {
+    public static final String LOG_TAG = "myLogs";
+    public static final int DAY = 1;
+    public static final int NIGHT = 2;
+}
