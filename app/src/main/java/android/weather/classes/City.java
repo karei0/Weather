7b21f67e99aed8ddd7587894b2af7afce1508f5c
sergius-m <p37sergius@gmail.com>
2014-02-28 17:46:26 +0300

    City class

---
+++ app/src/main/java/android/weather/classes/City.java
@@ -0,0 +1,38 @@
+package android.weather.classes;
+
+
+public class City {
+    private int cityId;
+    private String cityName;
+    private String country;
+
+    public City(int cityId, String cityName, String country) {
+        this.cityId = cityId;
+        this.cityName = cityName;
+        this.country = country;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
